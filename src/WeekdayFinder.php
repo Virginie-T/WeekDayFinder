9592fc353d862ac4c5a1b9503852df23557b1152
Jill Kuchman & virginie trubiano <student@epicodus.com>
2015-03-12 16:22:04 -0700

    add next day method

--- src/WeekdayFinder.php
+++ src/WeekdayFinder.php
@@ -6,15 +6,26 @@
         function find_the_weekday($input_year, $input_month, $input_day)
         {
 
+            $anchor_year = 2015;
+            $anchor_month = 3;
+            $anchor_day = 12;
 
+            $year_days = $input_year - $anchor_year;
+            $month_days = $input_month - $anchor_month;
+            $day_days = $input_day - $anchor_day;
 
+            $total_days = $year_days + $month_days + $day_days;
 
-            $output="";
-            if ($input_year == 2015 && $input_month == 03 && $input_day == 12) {
-                $output='Thursday';
-            } else {
-                $output='FALSE';
+            $modulo_days = $total_days%7;
+            switch ($modulo_days) {
+                case 0:
+                    $output = 'Thursday';
+                    break;
+                case 1:
+                    $output = 'Friday';
+                    break;
             }
+
             return $output;
         }
 
